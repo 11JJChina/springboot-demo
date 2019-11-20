4e536cb80ba4a5a705ed389b8b938b90ee75b652
ThinkPad <13120739083@163.com>
2019-11-20 13:50:32 +0800

    代码简单修改

--- src/main/java/com/example/springbootdemo/controller/DemoController.java
+++ src/main/java/com/example/springbootdemo/controller/DemoController.java
@@ -3,6 +3,8 @@ package com.example.springbootdemo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static sun.rmi.transport.TransportConstants.Return;
+
 @RestController
 public class DemoController {
 
@@ -10,6 +12,11 @@ public class DemoController {
     @RequestMapping("/hello")
     public String helloWrold(){
         return "Hello World！！！！";
+    }
+
+    @RequestMapping("/test")
+    public String testJenkis(){
+        return "你好呀，Jenkins!!!!";
     }
 
 
